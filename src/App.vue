63bda290ce61c207d883ba42672a3316cadd69f0
Shehu <shehu.adams@gmail.com>
2019-02-12 14:39:13 +0100

    Added header & Crypto table component .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <AppHeader />
+    <nav id="nav">
+      <div class="nav-item">
+        Market cap:
+      </div>
+      <div class="nav-item">
+        24h Vol cap:
+      </div>
+      <div class="nav-item">
+        Btc Dominance:
+      </div>
+    </nav>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    </div> -->
+    <router-view />
   </div>
 </template>
 
+<script>
+import AppHeader from './views/AppHeader'
+export default {
+  name: 'app',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+
+  #nav {
+    display: flex;
+    padding: 30px;
+    justify-content: space-evenly;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
 }
---
+++ src/components/CryptoTable.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Crypto table component
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>

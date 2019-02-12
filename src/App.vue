632e699a9d2c524674140deaaa9dee0bea703b2b
Shehu <shehu.adams@gmail.com>
2019-02-12 14:49:39 +0100

    Moved nav from Root to header component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
 <template>
   <div id="app">
     <AppHeader />
-    <nav id="nav">
-      <div class="nav-item">
-        Market cap:
-      </div>
-      <div class="nav-item">
-        24h Vol cap:
-      </div>
-      <div class="nav-item">
-        Btc Dominance:
-      </div>
-    </nav>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
     <router-view />
   </div>
 </template>
@@ -31,25 +16,17 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-
-  #nav {
-    display: flex;
-    padding: 30px;
-    justify-content: space-evenly;
-
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
-  }
 }
 </style>
--- src/views/AppHeader.vue
+++ src/views/AppHeader.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    <h3>Header</h3>
+  <div id="app-header">
+    <h3>Top 100 Cryptos</h3>
+
+    <nav id="nav">
+      <div class="nav-item">
+        Market cap:
+      </div>
+      <div class="nav-item">
+        24h Vol cap:
+      </div>
+      <div class="nav-item">
+        Btc Dominance:
+      </div>
+    </nav>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
   </div>
 </template>
 
@@ -11,4 +27,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#app-header {
+  h3 {
+    margin: 1rem 0;
+  }
+  #nav {
+    display: flex;
+    padding: 1rem;
+    justify-content: space-evenly;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
+}
 </style>
